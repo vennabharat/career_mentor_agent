0377f82fcaf54a9d5143dcac0e02bf0044cf38d4
Venna Bharat <142382354+vennabharat@users.noreply.github.com>
2026-06-30 01:33:44 +0530

    embeddings
    generating embeddings locally using sentence transformer.

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,6 @@
 from google import genai    # importing genai from google for generating embeddings
 from dotenv import load_dotenv  # importing dotenv for loading API key from .env file
+from sentence_transformers import SentenceTransformer  # for generating embeddings locally
 
 import os 
 
@@ -8,12 +9,24 @@ load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))  # calling api key for creating embeddings
 
 # Create embedding from string, returns embedding
-def get_embedding(text: str):
+"""def get_embedding(text: str):
     response = client.models.embed_content(
         model="gemini-embedding-2", 
         contents=text
     )
-    return response.embeddings[0].values
+    return response.embeddings[0].values"""
+    
+# Create embedding from string using local model with sentence transformer, returns embedding
+def get_embedding(text: str):
+    model = SentenceTransformer(
+        "all-MiniLM-L6-v2"
+    )
+    
+    response = model.encode(
+        text
+    )
+    
+    return response
 
 # Create embeddings from documents, return [] embeddings
 def get_embeddings(document):
